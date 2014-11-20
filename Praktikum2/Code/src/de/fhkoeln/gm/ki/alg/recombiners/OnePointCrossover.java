6a72051fd7f3b92dc240ed6b31f90faffa0be645
Felix Gebauer <gebauer.wiehl@web.de>
2014-11-20 15:44:29 +0100

    OnePointCrossover bearbeitet.

--- Praktikum2/Code/src/de/fhkoeln/gm/ki/alg/recombiners/OnePointCrossover.java
+++ Praktikum2/Code/src/de/fhkoeln/gm/ki/alg/recombiners/OnePointCrossover.java
@@ -2,7 +2,9 @@ package de.fhkoeln.gm.ki.alg.recombiners;
 
 import java.util.ArrayList;
 import java.util.Random;
+
 import de.fhkoeln.gm.ki.alg.genes.AbstractGene;
+import de.fhkoeln.gm.ki.alg.util.Individual;
 import de.fhkoeln.gm.ki.alg.util.Population;
 
 public class OnePointCrossover extends AbstractRecombiner {
@@ -15,12 +17,23 @@ public class OnePointCrossover extends AbstractRecombiner {
 
 	@Override
 	public Population recombine(Population currentPopulation) {
-		int crossPoint = new Random().nextInt(5)+1;
-		for (int i = 0; i < (currentPopulation.getCurrentSize()/2); i++) {
-			ArrayList<AbstractGene> fatherGenes = currentPopulation.getIndividualAt(i*2).getGenes();
-			ArrayList<AbstractGene> motherGenes = currentPopulation.getIndividualAt(i*2+1).getGenes();
-			ArrayList<AbstractGene> childOne = (ArrayList<AbstractGene>) fatherGenes.subList(0, crossPoint);
-			ArrayList<AbstractGene> childTwo
+		
+		int crossPoint = new Random().nextInt(5) + 1;
+		
+		for (int i = 0; i < (currentPopulation.getCurrentSize() / 2); i++) {
+			
+			ArrayList<AbstractGene> fatherGenes = currentPopulation.getIndividualAt(i * 2).getGenes();
+			ArrayList<AbstractGene> motherGenes = currentPopulation.getIndividualAt(i * 2 + 1).getGenes();
+			
+			ArrayList<AbstractGene> childOneGenes = (ArrayList<AbstractGene>) fatherGenes.subList(0, crossPoint);
+			childOneGenes.addAll(motherGenes.subList(crossPoint, 7));
+			
+			ArrayList<AbstractGene> childTwoGenes = (ArrayList<AbstractGene>) motherGenes.subList(0, crossPoint);
+			childTwoGenes.addAll(motherGenes.subList(crossPoint, 7));
+			
+			currentPopulation.replace(currentPopulation.getIndividualAt(i * 2), new Individual(childOneGenes));
+			currentPopulation.replace(currentPopulation.getIndividualAt(i * 2 + 1), new Individual(childTwoGenes));
+			
 		}
 		
 		return currentPopulation;

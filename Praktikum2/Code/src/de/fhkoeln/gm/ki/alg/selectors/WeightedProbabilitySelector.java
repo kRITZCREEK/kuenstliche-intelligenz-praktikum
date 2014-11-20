33a96941aab3dd2b5140b9ec7a0c86e52d225a90
jaaanix <jsa@opitz-consulting.de>
2014-11-20 17:33:20 +0100

    fügt die Klasse WeightedProbabilitySelector.java hinzu

--- Praktikum2/Code/src/de/fhkoeln/gm/ki/alg/selectors/WeightedProbabilitySelector.java
+++ Praktikum2/Code/src/de/fhkoeln/gm/ki/alg/selectors/WeightedProbabilitySelector.java
@@ -1,19 +1,34 @@
 package de.fhkoeln.gm.ki.alg.selectors;
 
+import de.fhkoeln.gm.ki.alg.util.Individual;
 import de.fhkoeln.gm.ki.alg.util.Population;
 
 public class WeightedProbabilitySelector extends AbstractSelector {
 
+	public static final double SURVIVALRATE = 0.9;
 	@Override
 	public Population selectFromPopulation(Population currentGen) {
-		// TODO Auto-generated method stub
-		return null;
+		Population population = new Population();
+		currentGen.sort();
+		double sumFitness = currentGen.getAverageFitness()*currentGen.getCurrentSize();
+		for(int i = 0; i < (int) (currentGen.getCurrentSize() * SURVIVALRATE); i++){
+			double stopVal = sumFitness * Math.random();
+			double accumulator = 0;
+			for(int j = 0; j < currentGen.getCurrentSize(); j++){
+				Individual individual = currentGen.getIndividualAt(j);
+				accumulator += individual.fitness;
+				if (accumulator >= stopVal){
+					population.add(individual);
+					break;
+				}
+			}						
+		}
+		return population;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "WeightedProbabilitySelector";
 	}
 
 }

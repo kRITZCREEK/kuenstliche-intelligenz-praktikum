3e8208954adb3c7cf4f9a954bd7dd51d3b35c1e7
kRITZCREEK <christoph.hegemann@gmx.net>
2015-01-22 12:04:07 +0100

    adds FeatureDetection

--- Praktikum3/Code/C3PO/src/Mapper.java
+++ Praktikum3/Code/C3PO/src/Mapper.java
@@ -46,7 +46,7 @@ public class Mapper {
 		Pose curPose = new Pose();
 		RangeReadings rR = null; 
 		
-		LineMap map = new LineMap();
+		LineMap map = new LineMap(scanneUmgebung(), new Rectangle(1000.0f, 1000.0f, 1000.0f, 1000.0f));
 		MCLPoseProvider provider = new MCLPoseProvider(mp, scanner, map , numParticles, border);
 		provider.generateParticles();
 		//provider.getReadings();
@@ -59,6 +59,106 @@ public class Mapper {
 			System.out.println(rR);
 		}
 		return map;
+	}
+	
+	
+private Line[] scanneUmgebung() {
+		
+		int POINTS = 18;
+		int ANGLEPERPOINT = 360 / POINTS;
+		
+		List<Line> rtn = new ArrayList<Line>();
+		
+		UltrasonicSensor USS = new UltrasonicSensor(SensorPort.S2);
+		RangeFeatureDetector detector = new RangeFeatureDetector(USS, 100, 250);
+		
+		int angle = 0;
+		float firstrange = -1;
+		float secondrange = -1;
+		
+		while(angle < POINTS) {
+			Feature f = detector.scan();
+			if(f == null)
+				continue;
+			try {
+				if (firstrange == -1) 
+					firstrange = f.getRangeReading().getRange();
+				else if (secondrange == -1)
+					secondrange = f.getRangeReading().getRange();
+				else {
+					
+					int currentangle = (angle - 1) * ANGLEPERPOINT;
+					int lastangle = (angle - 2) * ANGLEPERPOINT;
+					
+					rtn.add(this.erzeugeLinie(currentangle, secondrange, lastangle, firstrange));
+					firstrange = -1;
+					secondrange = -1;
+				}
+				
+			} finally {
+				
+			}
+			Motor.A.rotate(20);
+			angle++;
+		}
+		for (int i = 0; i < rtn.size(); i++) {
+			System.out.println("(" + rtn.get(i).x1 + "|" + rtn.get(i).y1 + ")(" + rtn.get(i).x2 + "|" + rtn.get(i).y2 + ")");
+		}
+		Motor.A.rotate(-360);
+		
+		return (Line[]) rtn.toArray();
+	}
+	
+	private Line erzeugeLinie(float range1, float angle1, float range2, float angle2) {
+		float[] p1 = berechneKoordinaten(range1, angle1);
+		float[] p2 = berechneKoordinaten(range2, angle2);
+		
+		return new Line(p1[0],p1[1],p2[0],p2[1]);
+	}
+	
+	private float[] berechneKoordinaten(float range, float angle) {
+		// rtn[0] = x 
+		// rtn[1] = y
+		
+		double a,b,c;
+		double alpha,
+			  beta,
+			  gamma;
+
+		if (angle < 91) {
+			c = range;
+			beta = angle;
+			gamma = 90;
+			alpha = 180 - (gamma + beta);
+			a = Math.sin(alpha) * c;
+			b = Math.cos(alpha) * c;
+			return new float[] { (float)a, (float)b };
+		} else if (angle < 181) {
+			c = range;
+			beta = angle - 90;
+			gamma = 90;
+			alpha = 180 - (gamma + beta);
+			a = Math.sin(alpha) * c;
+			b = Math.cos(alpha) * c;
+			return new float[] { (float)a, -(float)b };
+		} else if (angle < 271) {
+			c = range;
+			beta = angle - 180;
+			gamma = 90;
+			alpha = 180 - (gamma + beta);
+			a = Math.sin(alpha) * c;
+			b = Math.cos(alpha) * c;
+			return new float[] { -(float)a, -(float)b };
+		} else {
+			c = range;
+			beta = angle - 270;
+			gamma = 90;
+			alpha = 180 - (gamma + beta);
+			a = Math.sin(alpha) * c;
+			b = Math.cos(alpha) * c;
+			return new float[] { -(float)a, (float)b };
+		}
+		
 	}
 	
 }

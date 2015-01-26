aa6f0a17bbccdddac8463344c90cdb9102e34868
kRITZCREEK <christoph.hegemann@gmx.net>
2015-01-26 12:14:04 +0100

    Initial Logger

---
+++ Praktikum3_Logger/src/Connector.java
@@ -0,0 +1,62 @@
+
+import lejos.nxt.Motor;
+import lejos.nxt.remote.NXTCommand;
+import lejos.pc.comm.NXTComm;
+import lejos.pc.comm.NXTCommLogListener;
+import lejos.pc.comm.NXTCommandConnector;
+import lejos.pc.comm.NXTConnector;
+
+/**
+ * Simple NXT BT-connector
+ * 
+ * Based on the example by Lawrie Griffiths and Brian Bagnall
+ * 
+ * This REQUIRES lejos NXJ!
+ * http://www.lejos.org/nxj-downloads.php
+ * 
+ * @author Christoph Hegemann
+ */
+public class Connector {
+	
+	private static NXTConnector conn;
+	private static boolean connected = false;
+	
+	public static void connect(String name) throws Exception {
+		conn = new NXTConnector();
+		conn.addLogListener(new NXTCommLogListener() {
+			public void logEvent(String message) {
+				System.out.println(message);				
+			}
+
+			public void logEvent(Throwable throwable) {
+				System.err.println(throwable.getMessage());			
+			}			
+		});
+		conn.setDebug(true);
+		if (!conn.connectTo("btspp://"+name, NXTComm.LCP)) {
+			System.err.println("Failed to connect");
+			System.exit(1);
+		}
+		NXTCommandConnector.setNXTCommand(new NXTCommand(conn.getNXTComm()));
+		connected=true;
+
+		Motor.A.setSpeed(20);
+		Motor.A.rotate(90);
+		
+		Motor.B.setSpeed(20);
+		Motor.C.setSpeed(20);
+
+	}	
+	
+	public static void close() throws Exception{
+		if(connected) {
+			conn.close();
+			Motor.A.rotate(-90);
+		}
+		connected=false;
+	}
+	
+	public static boolean isConnected(){
+		return connected;
+	}
+}
---
+++ Praktikum3_Logger/src/RobotLogger.java
@@ -0,0 +1,108 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import lejos.geom.Line;
+import lejos.geom.Point;
+import lejos.geom.Rectangle;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.RangeFinder;
+import lejos.robotics.RangeReading;
+import lejos.robotics.RangeReadings;
+import lejos.robotics.RangeScanner;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.RotatingRangeScanner;
+import lejos.robotics.localization.MCLPoseProvider;
+import lejos.robotics.mapping.LineMap;
+import lejos.robotics.mapping.RangeMap;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.MoveProvider;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.Pose;
+import lejos.robotics.objectdetection.Feature;
+import lejos.robotics.objectdetection.RangeFeatureDetector;
+import lejos.robotics.pathfinding.Path;
+
+
+
+public class RobotLogger {	
+	
+	public static void main(String[] args) throws IOException{
+		System.out.println("C3PO");
+		RobotLogger rl = new RobotLogger();
+		rl.map();
+	}
+
+	private RegulatedMotor head = Motor.A;
+	private UltrasonicSensor sensor = new UltrasonicSensor(SensorPort.S2);
+	private RangeScanner scanner = new RotatingRangeScanner(head, sensor);
+	private DifferentialPilot mp = new DifferentialPilot(59, 120, Motor.B, Motor.C);
+	
+	
+	private void map() throws IOException{
+		long initialTime = System.nanoTime();
+		
+		FileOutputStream fos_motor = new FileOutputStream("motor_log.txt"); 
+		OutputStreamWriter out_motor = new OutputStreamWriter(fos_motor, "UTF-8");
+		
+		FileOutputStream fos_uss = new FileOutputStream("uss_log.txt"); 
+		OutputStreamWriter out_uss = new OutputStreamWriter(fos_uss, "UTF-8");
+		
+		StringBuilder sb_motor = new StringBuilder("");
+		StringBuilder sb_uss = new StringBuilder("");
+		
+		float [] angles = new float[36]; 
+		for(int i = 0; i < 36; i++){
+			if (i==0){
+				angles[i] = 0.0f;
+			}
+			else{
+				angles[i] = angles[i-1] + 10.0f;
+			}
+				
+		}
+		scanner.setAngles(angles);
+		Pose curPose = new Pose();
+		RangeReadings rR = null;
+
+		mp.setTravelSpeed(50.0);
+		
+		for(int j = 0; j < 2; j++){
+			rR = scanner.getRangeValues();
+			sb_uss.append("S ");
+			for(RangeReading r : rR){
+				System.out.println(r.getAngle() + ", " + r.getRange());
+				sb_uss.append(Math.round((r.getRange()*10)) + " ");
+			}
+			sb_uss.append("\n");
+			System.out.println("Motor B: " + Motor.B.getTachoCount());
+			System.out.println("Motor C: " + Motor.C.getTachoCount());
+			
+			String out_string_motor = "M" + " " 
+							  + (System.nanoTime() - initialTime) / 1000000 + " "
+					          + Motor.C.getTachoCount() + " "
+					          + "a a a " +
+					          + Motor.B.getTachoCount() + " "
+					          + "a a a a a a a\n";
+			sb_motor.append(out_string_motor); 
+							
+			mp.travel(100.0);
+			
+			System.out.println("Motor B: " + Motor.B.getTachoCount());
+			System.out.println("Motor C: " + Motor.C.getTachoCount());
+			
+		}
+		out_motor.write(sb_motor.toString());
+		out_motor.flush();
+		out_motor.close();
+		
+		out_uss.write(sb_uss.toString());
+		out_uss.flush();
+		out_uss.close();
+	
+	}
+}
